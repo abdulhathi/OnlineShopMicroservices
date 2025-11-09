e715d65ef2973bb09593fd5dc4541d9da4696f2e
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2025-11-09 10:32:29 -0800

    Using icommand and I commandhandler from building blocks

--- src/Services/Catalog/CatalogAPI/Product/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Product/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,12 @@
-using MediatR;
+using BuildingBlocks.CQRS;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
   public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
   {
