1eacfc5ea1a8071ca16be485d00ee1baa6f6d621
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2025-11-09 14:09:45 -0800

    Add a mapster pacakge in building blocks. And Product enpoint using MedatR to create a request and mapster to map the objects.

--- src/Services/Catalog/CatalogAPI/Product/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Product/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,8 @@
 using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
 
@@ -8,6 +12,20 @@ public class CreateProductEndpoint : ICarterModule
 {
   public void AddRoutes(IEndpointRouteBuilder app)
   {
-    throw new NotImplementedException();
+    app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+    {
+      var command = request.Adapt<CreateProductCommand>();
+
+      var result = await sender.Send(command);
+
+      var response = result.Adapt<CreateProductResponse>();
+
+      return Results.Created($"/products/{response.Id}", response);
+    })
+    .WithName("CreateProduct")
+    .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+    .ProducesProblem(StatusCodes.Status400BadRequest)
+    .WithSummary("Create Product")
+    .WithDescription("Create Product");
   }
 }
