68d3c5805ebaaa9793cd090d253fc6b4c30918d6
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2025-11-09 10:28:47 -0800

    Implementing the ICommand, IQuery, ICommandHanlder and IQueryHandler for following the CQRS pattern using the MediatR

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,9 @@
+using BuildingBlocks.CQRS;
+using MediatR;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+where TQuery : IQuery<TResponse>
+where TResponse : notnull
+{
+
+}
