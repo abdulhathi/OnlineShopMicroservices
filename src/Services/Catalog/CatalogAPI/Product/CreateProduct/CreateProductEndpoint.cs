faa343effc297246ac0b69cabae1ff8bc85e486f
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2025-11-09 13:46:33 -0800

    Inherit ICartModule on Createproduct endpoint and implement the interface

--- src/Services/Catalog/CatalogAPI/Product/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Product/CreateProduct/CreateProductEndpoint.cs
@@ -1,8 +1,13 @@
+using Carter;
+
 public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
 
 public record CreateProductResponse(Guid Id);
 
-public class CreateProductEndpoint
+public class CreateProductEndpoint : ICarterModule
 {
-
+  public void AddRoutes(IEndpointRouteBuilder app)
+  {
+    throw new NotImplementedException();
+  }
 }
